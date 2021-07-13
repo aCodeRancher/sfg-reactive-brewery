ba1c1a510269fd320eccb45916bb31b7368d834f
helen <hhma@alumni.cpp.edu>
2021-07-13 14:31:53 -0400

    add test for getBeerByUpc and listBeers.

--- src/test/java/guru/springframework/sfgrestbrewery/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/sfgrestbrewery/web/controller/BeerControllerTest.java
@@ -3,19 +3,26 @@ package guru.springframework.sfgrestbrewery.web.controller;
 import guru.springframework.sfgrestbrewery.bootstrap.BeerLoader;
 import guru.springframework.sfgrestbrewery.services.BeerService;
 import guru.springframework.sfgrestbrewery.web.model.BeerDto;
+import guru.springframework.sfgrestbrewery.web.model.BeerPagedList;
+import guru.springframework.sfgrestbrewery.web.model.BeerStyleEnum;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 @WebFluxTest(BeerController.class)
 class BeerControllerTest {
@@ -51,20 +58,33 @@ class BeerControllerTest {
                 .value(beerDto -> beerDto.getBeerName(), equalTo(validBeer.getBeerName()));
     }
 
+    @Test
+    void getBeerList(){
+        List<BeerDto> content = new ArrayList<>();
+        content.add(validBeer);
+        BeerPagedList beerList = new BeerPagedList(content);
+        when(beerService.listBeers(anyString(), any(BeerStyleEnum.class), any(PageRequest.class),anyBoolean())).thenReturn(beerList);
 
+        webTestClient.get()
+                .uri("/api/v1/beer?pageNumber=1&pageSize=1&beerName=Galaxy&beerStyle=ALE&showInventoryOnHand=false")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(BeerPagedList.class)
+                .value(beerPageList -> beerPageList.toList().size(), equalTo(1));
+    }
 
+    @Test
+    void getBeerByUpc(){
 
-
-
-
-
-
-
-
-
-
-
-
-
+       when(beerService.getByUpc(any())).thenReturn(validBeer);
+       webTestClient.get()
+                .uri("/api/v1/beerUpc/" + validBeer.getUpc())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(BeerDto.class)
+                .value(beerDto -> beerDto.getBeerName(), equalTo(validBeer.getBeerName()));
+    }
 
 }
